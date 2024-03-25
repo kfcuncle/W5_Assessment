c8bb73f2678fd5acf40caef2c72955f804184a07
Tan Jia He <jiahe.tan@bertelsmann.de>
2024-03-25 11:35:28 +0800

    [W5_Asssessment]-first Commit]

---
+++ src/main/java/com/example/W5Assessment/W5AssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.W5Assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W5AssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W5AssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/W5Assessment/W5AssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.W5Assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W5AssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
